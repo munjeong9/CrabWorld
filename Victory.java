9ed958295919513a7db8861912010cc4c7fc4d33
munjeong9 <answjd-119@hanmail.net>
2016-11-20 00:30:58 +0900

    Update Victory.java

--- Victory.java
+++ Victory.java
@@ -1,5 +1,19 @@
-package game;
-
-public class Victory {
-
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+/**
+ * Write a description of class Victory here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Victory extends Actor
+{
+    /**
+     * Act - do whatever the Victory wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        setImage(new GreenfootImage("Victory", 50, Color.BLUE, Color.WHITE));
+    }    
 }
