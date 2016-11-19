2a9c2b8b5e3b93aae15788b9cff8c94013cc45c4
munjeong9 <answjd-119@hanmail.net>
2016-11-20 00:31:32 +0900

    Update GameOver.java

--- GameOver.java
+++ GameOver.java
@@ -1,5 +1,19 @@
-package game;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class GameOver {
-
+/**
+ * Write a description of class GameOver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOver extends Actor
+{
+    /**
+     * Act - do whatever the GameOver wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public GameOver() 
+    {
+       
+    }    
 }
